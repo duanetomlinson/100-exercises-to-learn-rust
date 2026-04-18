aec6924e7d4039d3da383bc555eb4ea70b1510f4
Alpha Brosive <140975496+duanetomlinson@users.noreply.github.com>
2026-04-18 16:49:32 -0400

    finished 2.5

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -1,6 +1,15 @@
 // Define a function named `factorial` that, given a non-negative integer `n`,
 // returns `n!`, the factorial of `n`.
 //
+fn factorial(_n: u32) -> u32 {
+    if _n == 0 {
+        1
+    } else {
+        _n * factorial(_n - 1)
+    }
+}
+
+//
 // The factorial of `n` is defined as the product of all positive integers up to `n`.
 // For example, `5!` (read "five factorial") is `5 * 4 * 3 * 2 * 1`, which is `120`.
 // `0!` is defined to be `1`.
