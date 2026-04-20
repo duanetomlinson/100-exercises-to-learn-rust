ee2d18bcab48bf52cd302d98d65367593dd629c0
Alpha Brosive <140975496+duanetomlinson@users.noreply.github.com>
2026-04-20 13:34:50 -0400

    finished 2.7 for loop

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,7 +1,18 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut answer: u32 = 1;
+
+    if n == 0 {
+        answer
+    } else {
+        for i in 1..=n {
+            answer = i * answer;
+        }
+        answer
+    }
 }
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
